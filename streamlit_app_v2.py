0b2828f7080d5a29b62f674dae67784a2f6adad1
wadebjackson1977-rgb <wadebjackson1977@gmail.com>
2026-07-02 16:44:36 +1000

    Update streamlit_app_v2.py

--- streamlit_app_v2.py
+++ streamlit_app_v2.py
@@ -84,4 +84,4 @@ st.dataframe(df.style.format({
 
 st.markdown("---")
 st.write("**Note:** This projection accounts for annual fee/insurance erosion and contribution taxes. Remember to review Division 296 rules if your nominal balance crosses $3,000,000.")
-st.caption("The prepopulated Fees and Insurance figures are indicative of a QPS officer with a Qsuper Account.")
+st.write("**Note:** The prepopulated Fees and Insurance figures are indicative of a QPS officer with a Qsuper Account.")
